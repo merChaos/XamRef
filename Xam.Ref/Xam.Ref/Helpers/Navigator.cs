e9948d32e2803ad271295c7fe5985d5a7e123865
merChaos <merChaos@users.noreply.github.com>
2016-05-05 15:15:03 -0500

    Update Navigator.cs
    Navigation Changes

--- Xam.Ref/Xam.Ref/Helpers/Navigator.cs
+++ Xam.Ref/Xam.Ref/Helpers/Navigator.cs
@@ -11,12 +11,20 @@ namespace Xam.Ref.Helpers
 {
     public class Navigator: INavigator
     {
-        private INavigation _navigation = null;
+        private static INavigation _navigation = null;
 
         public Task NavigateToAsync<T>(object args = null)
         {
+            if (_navigation == null) throw new ArgumentNullException("INavigation");
+
             var page = IocContainer.Resolve<IPageLocator>().ResolvePage(typeof(T), args);
             return _navigation.PushAsync(page);
+        }
+
+        public INavigator SetNavigator(object platformNavigator)
+        {
+            _navigation = (INavigation)platformNavigator;
+            return this;
         }
     }
 }
